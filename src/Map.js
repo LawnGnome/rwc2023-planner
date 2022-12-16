55af21ffe46ae256d78769282aa0192650576ac1
Adam Harvey <adam@adamharvey.name>
2022-12-16 12:34:42 -0800

    Update the tile server URL.

--- src/Map.js
+++ src/Map.js
@@ -85,8 +85,8 @@ export default class Map extends React.Component {
               maxZoom={18}
             >
               <TileLayer
-                url="http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}{r}.png"
                 attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+                url="https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png"
               />
               {Object.entries(grounds).map(([label, ground]) => {
                 const di = L.divIcon({
